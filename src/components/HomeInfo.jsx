2abef0881d16bf30520bb6d8f33f9e318d6623e0
sgr-0007 <sagarhs0007@gmail.com>
2024-02-11 13:08:53 +0000

    modified homeinfo

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
         <span className='font-semibold mx-2 text-white'>Sagar</span>
         👋
         <br />
-        A Software Engineer from India
+        A Software Engineer.
       </h1>
     );
 
